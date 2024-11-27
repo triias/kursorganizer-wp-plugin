d1087cc731088f4eaecb845d614559f7d154ee0c
Matthias <querformat@gmail.com>
2024-11-27 16:55:12 +0100

    Version 1.0.0

---
+++ CHANGELOG.md
@@ -0,0 +1,18 @@
+# Changelog
+
+## [1.0.0] - 2024-11-27
+### Added
+- Initial release of the KursOrganizer X iFrame plugin.
+- Added shortcode `[kursorganizer_iframe]` to embed the KursOrganizer web module in WordPress pages.
+- Implemented settings page for configuring the plugin.
+- Added support for filtering courses by city, instructor, course type, location, day, and category.
+- Included debug mode for displaying technical information under the iFrame.
+
+### Fixed
+- N/A
+
+### Changed
+- N/A
+
+### Removed
+- N/A
---
+++ includes/class-plugin-updater.php
@@ -0,0 +1,143 @@
+<?php
+// includes/class-plugin-updater.php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class KursOrganizer_Plugin_Updater
+{
+    private $file;
+    private $plugin;
+    private $basename;
+    private $active;
+    private $github_response;
+    private $authorize_token;
+    private $github_url;
+
+    public function __construct($file)
+    {
+        $this->file = $file;
+        $this->plugin = plugin_basename($file);
+        $this->basename = plugin_basename($file, '.php');
+        $this->active = is_plugin_active($this->plugin);
+        $this->github_url = 'https://api.github.com/repos/[DEIN-USERNAME]/kursorganizer-iframe';
+
+        // Hook in die WordPress Update-Prozesse
+        add_filter('pre_set_site_transient_update_plugins', array($this, 'modify_transient'), 10, 1);
+        add_filter('plugins_api', array($this, 'plugin_popup'), 10, 3);
+        add_filter('upgrader_post_install', array($this, 'after_install'), 10, 3);
+
+        // Setze GitHub Access Token
+        $this->authorize_token = get_option('kursorganizer_github_token');
+    }
+
+    public function modify_transient($transient)
+    {
+        if (!$transient || !isset($transient->checked)) {
+            return $transient;
+        }
+
+        // Hole GitHub Release-Informationen
+        $this->get_repository_info();
+
+        // Wenn eine neue Version verfügbar ist
+        if (
+            $this->github_response
+            && version_compare($this->github_response->tag_name, $transient->checked[$this->plugin], '>')
+        ) {
+            $plugin = array(
+                'url' => $this->plugin,
+                'slug' => $this->basename,
+                'package' => $this->github_response->zipball_url,
+                'new_version' => $this->github_response->tag_name
+            );
+            $transient->response[$this->plugin] = (object) $plugin;
+        }
+
+        return $transient;
+    }
+
+    public function plugin_popup($result, $action, $args)
+    {
+        if ($action !== 'plugin_information') {
+            return $result;
+        }
+
+        if (!empty($args->slug)) {
+            if ($args->slug == $this->basename) {
+                $this->get_repository_info();
+
+                $plugin = array(
+                    'name'              => $this->plugin,
+                    'slug'              => $this->basename,
+                    'version'           => $this->github_response->tag_name,
+                    'author'            => 'KursOrganizer GmbH',
+                    'author_profile'    => 'https://github.com/[DEIN-USERNAME]',
+                    'last_updated'      => $this->github_response->published_at,
+                    'homepage'          => $this->github_response->html_url,
+                    'short_description' => $this->github_response->description,
+                    'sections'          => array(
+                        'Description'   => $this->github_response->description,
+                        'Updates'       => $this->get_changelog(),
+                    ),
+                    'download_link'     => $this->github_response->zipball_url
+                );
+
+                return (object) $plugin;
+            }
+        }
+
+        return $result;
+    }
+
+    private function get_repository_info()
+    {
+        if (is_null($this->github_response)) {
+            $args = array();
+            if ($this->authorize_token) {
+                $args['headers']['Authorization'] = "token {$this->authorize_token}";
+            }
+
+            $response = wp_remote_get(
+                "{$this->github_url}/releases/latest",
+                $args
+            );
+
+            if (is_wp_error($response)) {
+                return false;
+            }
+
+            $this->github_response = json_decode($response['body']);
+        }
+    }
+
+    private function get_changelog()
+    {
+        $response = wp_remote_get(
+            "{$this->github_url}/raw/master/CHANGELOG.md",
+            array('headers' => array('Authorization' => "token {$this->authorize_token}"))
+        );
+
+        if (is_wp_error($response)) {
+            return 'Keine Änderungshistorie verfügbar.';
+        }
+
+        return $response['body'];
+    }
+
+    public function after_install($response, $hook_extra, $result)
+    {
+        global $wp_filesystem;
+
+        $install_directory = plugin_dir_path($this->file);
+        $wp_filesystem->move($result['destination'], $install_directory);
+        $result['destination'] = $install_directory;
+
+        if ($this->active) {
+            activate_plugin($this->plugin);
+        }
+
+        return $result;
+    }
+}
